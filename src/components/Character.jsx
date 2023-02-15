48acb1fe53f4017950e96fb2678a44d7e241a0c3
OksanaFedyukova <avseniya222@gmail.com>
2023-02-15 09:35:38 +0100

    reducer

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,9 +1,25 @@
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer, useMemo} from 'react';
 
+const initialState={
+  favorites: []
+}
 
+const favoriteReducer = (state,action)=>{
+  switch (action.type){
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites:[...state.favorite, action.payload],
+      }
+      default:
+        return state
+  }
+}
 function Character() {
     const[characters, setCharacters] = useState([]);
+    const [favorites,dispatch] = useReducer(favoriteReducer, initialState);
+    const[search, setSearch]=useState("");
 
      useEffect(()=>{
     fetch('https://rickandmortyapi.com/api/character')
@@ -11,14 +27,34 @@ function Character() {
     .then((data)=>setCharacters(data.results));
 
 }, []);
+   
+  const handleClick=(favorite)=>{
+    dispatch({
+      type:'ADD_TO_FAVORITE', payload:favorite
+    })
+  }
+   
+  const handleSearch = (e)=> {
+    setSearch(e.target.value)
+  }
 
+ const filterCharacters = useMemo(()=> 
+  characters.filter((char) => {
+    return char.name.toLowerCase().includes()
+  })
+ )
     return (
         <div>
+          {favorites.favorites.map((favorite)=>(
+ <li key={favorite.id}>{favorite.name}</li>
+          ))}
+          <input type="text" value={search} onChange={handleSearch} />
           {characters.map((character)=>{
             return(
                 <div key={character.id}>
                <h2>{character.name}</h2>
                <img src={character.image} alt="" />
+               <button onClick = {()=>handleClick(character)}>Add to my Favorite</button>
                 </div>
             )
           })
